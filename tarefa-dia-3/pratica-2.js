4c599c436744032e92449ac173028c112d6c704f
Rafael Martins <rafaelmartinsferreira03@gmail.com>
2022-08-27 12:21:47 -0300

    Tabuada de 5[C

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,10 @@
+let contador = 1;
+let numTabuada = 5;
+
+console.log("Tabuada do número" + numTabuada);
+
+
+while (contador <= 10) {
+    console.log(numTabuada + "x" + contador + "=", (numTabuada * contador));
+    contador ++
+}
